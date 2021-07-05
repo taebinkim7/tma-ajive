babf4c20dbf30572cd92fbe0444b6be5056739be
Taebin Kim <44902482+taebinkim7@users.noreply.github.com>
2021-07-05 11:35:54 -0400

    Update files

--- scripts/ajive.py
+++ scripts/ajive.py
@@ -35,54 +35,54 @@ ajive = ajive.fit({'he': subj_feats_he, 'er': subj_feats_er})
 
 dump(ajive, os.path.join(Paths().results_dir, 'data', 'fit_ajive'))
 
-#####################
-# AJIVE diagnostics #
-#####################
-
-# diagnostic plot
-plt.figure(figsize=[10, 10])
-ajive.plot_joint_diagnostic()
-savefig(os.path.join(Paths().results_dir, 'ajive_diagnostic.png'))
-
-#################
-# plot loadings #
-#################
-
-# set visualization configs
-mpl_noaxis(labels=True)
-
-n_genes = 90
-inches = 5
-height_scale = n_genes // 25
-load_figsize = (inches, height_scale * inches)
-
-# common loadings
-load_dir = os.path.join(Paths().results_dir, 'common', 'loadings')
-os.makedirs(load_dir, exist_ok=True)
-for r in range(ajive.common.rank):
-    plt.figure(figsize=load_figsize)
-    plt.plot(ajive.common.loadings(r))
-    #ajive.blocks['he'].plot_common_loading(r)
-    plt.title('common component {}'.format(r + 1))
-    savefig(os.path.join(load_dir, 'loadings_comp_{}.png'.format(r + 1)))
-
-
-# he individual loadings
-load_dir = os.path.join(Paths().results_dir, 'he_indiv', 'loadings')
-os.makedirs(load_dir, exist_ok=True)
-n_indiv_comps = min(5, ajive.blocks['he'].individual.rank)
-for r in range(n_indiv_comps):
-    plt.figure(figsize=load_figsize)
-    plt.plot(ajive.blocks['he'].individual.loadings(r))
-    plt.title('HE individual component {}'.format(r + 1))
-    savefig(os.path.join(load_dir, 'loadings_comp_{}.png'.format(r + 1)))
-
-# er individual loadings
-load_dir = os.path.join(Paths().results_dir, 'er_indiv', 'loadings')
-os.makedirs(load_dir, exist_ok=True)
-n_indiv_comps = min(5, ajive.blocks['er'].individual.rank)
-for r in range(n_indiv_comps):
-    plt.figure(figsize=load_figsize)
-    plt.plot(ajive.blocks['er'].individual.loadings(r))
-    plt.title('ER individual component {}'.format(r + 1))
-    savefig(os.path.join(load_dir, 'loadings_comp_{}.png'.format(r + 1)))
+# #####################
+# # AJIVE diagnostics #
+# #####################
+#
+# # diagnostic plot
+# plt.figure(figsize=[10, 10])
+# ajive.plot_joint_diagnostic()
+# savefig(os.path.join(Paths().results_dir, 'ajive_diagnostic.png'))
+#
+# #################
+# # plot loadings #
+# #################
+#
+# # set visualization configs
+# mpl_noaxis(labels=True)
+#
+# n_genes = 90
+# inches = 5
+# height_scale = n_genes // 25
+# load_figsize = (inches, height_scale * inches)
+#
+# # common loadings
+# load_dir = os.path.join(Paths().results_dir, 'common', 'loadings')
+# os.makedirs(load_dir, exist_ok=True)
+# for r in range(ajive.common.rank):
+#     plt.figure(figsize=load_figsize)
+#     plt.plot(ajive.common.loadings(r))
+#     #ajive.blocks['he'].plot_common_loading(r)
+#     plt.title('common component {}'.format(r + 1))
+#     savefig(os.path.join(load_dir, 'loadings_comp_{}.png'.format(r + 1)))
+#
+#
+# # he individual loadings
+# load_dir = os.path.join(Paths().results_dir, 'he_indiv', 'loadings')
+# os.makedirs(load_dir, exist_ok=True)
+# n_indiv_comps = min(5, ajive.blocks['he'].individual.rank)
+# for r in range(n_indiv_comps):
+#     plt.figure(figsize=load_figsize)
+#     plt.plot(ajive.blocks['he'].individual.loadings(r))
+#     plt.title('HE individual component {}'.format(r + 1))
+#     savefig(os.path.join(load_dir, 'loadings_comp_{}.png'.format(r + 1)))
+#
+# # er individual loadings
+# load_dir = os.path.join(Paths().results_dir, 'er_indiv', 'loadings')
+# os.makedirs(load_dir, exist_ok=True)
+# n_indiv_comps = min(5, ajive.blocks['er'].individual.rank)
+# for r in range(n_indiv_comps):
+#     plt.figure(figsize=load_figsize)
+#     plt.plot(ajive.blocks['er'].individual.loadings(r))
+#     plt.title('ER individual component {}'.format(r + 1))
+#     savefig(os.path.join(load_dir, 'loadings_comp_{}.png'.format(r + 1)))
