40a17158e44b96fd7f414af7505dee08c59d04a9
Taebin Kim <44902482+taebinkim7@users.noreply.github.com>
2021-07-06 20:04:29 -0400

    Add classification with ER features

---
+++ scripts/classification_er.py
@@ -0,0 +1,39 @@
+import os
+import numpy as np
+import pandas as pd
+
+from joblib import dump
+from tqdm import tqdm
+from tma_ajive.Paths import Paths
+from tma_ajive.load_image_feats import load_image_feats
+from tma_ajive.classification import base_classification
+
+
+data = load_image_feats(load_patch_data=False)
+feats_er = data['subj_feats_er']
+
+labels_file = os.path.join(Paths().classification_dir, 'subj_labels_er.csv')
+labels = pd.read_csv(labels_file, index_col=0)
+
+intersection = list(set(feats_er.index).intersection(set(labels.index)))
+feats_er = feats_er.loc[intersection]
+labels = labels.loc[intersection]
+
+test_acc_list = []
+for i in tqdm(range(100)):
+    n = len(labels)
+    perm_idx = np.random.RandomState(seed=None).permutation(np.arange(n))
+    train_idx, test_idx = perm_idx[:int(.8 * n)], perm_idx[int(.8 * n):]
+
+    train_feats, train_labels = feats_er.iloc[train_idx], labels.iloc[train_idx]
+    test_feats, test_labels = feats_er.iloc[test_idx], labels.iloc[test_idx]
+
+    train_dataset = [train_feats, train_labels]
+    test_dataset = [test_feats, test_labels]
+
+    _, test_acc = base_classification(train_dataset, test_dataset, 'dwd')
+    test_acc_list.append(test_acc)
+
+# dump(test_acc_list, os.path.join(Paths().classification_dir, 'test_acc_list'))
+mean_test_acc = np.mean(test_acc_list)
+print(mean_test_acc)
