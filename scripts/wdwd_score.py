33706bc451d01d502d8085b780ff66cb65758e57
Taebin Kim <44902482+taebinkim7@users.noreply.github.com>
2021-08-30 16:56:23 -0400

    Update files

--- scripts/wdwd_score.py
+++ scripts/wdwd_score.py
@@ -6,10 +6,11 @@ import seaborn as sns
 
 from joblib import dump
 from skimage.io import imread
-from patch_classifier import DWDClassifier
+from patch_classifier import WDWDClassifier
 from tma_ajive.load_analysis_data import load_analysis_data
 from tma_ajive.classification import get_train_test_ids
 from tma_ajive.Paths import Paths
+from tma_ajive.viz_utils import get_extreme_images
 
 data = load_analysis_data(load_patch_data=False)
 
@@ -19,28 +20,23 @@ dump(data, os.path.join(Paths().classification_dir, 'data'))
 feats = data['feats_er']
 labels = data['labels_er']
 
-# get balanced dataset to train classifier
-bal_ids = get_train_test_ids(labels, balanced=True)
-bal_feats = feats.loc[bal_ids]
-bal_labels = labels.loc[bal_ids]
+feats = feats.to_numpy()
+labels = labels['er_label'].to_numpy()
 
-bal_feats = bal_feats.to_numpy()
-bal_labels = bal_labels['er_label'].to_numpy()
-
-dwd_file = os.path.join(Paths().classification_dir, 'dwd_all')
-if os.path.isfile(dwd_file):
-    # load DWD if it exists
-    dwd = DWDClassifier.load(dwd_file)
+wdwd_file = os.path.join(Paths().classification_dir, 'wdwd_all')
+if os.path.isfile(wdwd_file):
+    # load wDWD if it exists
+    classifier = WDWDClassifier.load(wdwd_file)
 else:
-    # train DWD and save it
-    dwd = DWDClassifier().fit(bal_feats, bal_labels)
-    dump(dwd, os.path.join(Paths().classification_dir, 'dwd_all'))
+    # train wDWD and save it
+    classifier = WDWDClassifier().fit(feats, labels)
+    dump(classifier, os.path.join(Paths().classification_dir, 'wdwd_all'))
 
 # define variables for visualization
 feats = feats.to_numpy()
 labels = labels['er_label'].to_numpy()
-preds = dwd.predict(feats)
-scores = feats @ dwd.coef_.T
+preds = classifier.predict(feats)
+scores = feats @ classifier.coef_.T + classifier.intercept_
 scores = scores.reshape(-1)
 
 tp_idx = (labels == 1) & (preds == 1)
@@ -49,14 +45,14 @@ fp_idx = (labels == 0) & (preds == 1)
 tn_idx = (labels == 0) & (preds == 0)
 
 # make directory to save plots
-plot_dir = os.path.join(Paths().classification_dir, 'plots')
+plot_dir = os.path.join(Paths().classification_dir, 'wdwd_plots')
 os.makedirs(plot_dir, exist_ok=True)
 
-# plot DWD scores
+# plot wDWD scores
 n = len(ids)
-noise = np.random.RandomState(10).normal(scale=.1, size=n)
+noise = (np.arange(n) - n // 2) / 5000
 ax = plt.axes()
-ax.set_title('Scatterplot of DWD scores (jitter = N(0, 0.1))', fontsize=13, fontweight='bold')
+ax.set_title('Scatterplot of wDWD scores (jitter = ID)', fontsize=13, fontweight='bold')
 ax.scatter(scores[tp_idx | fn_idx], labels[tp_idx | fn_idx] + noise[tp_idx | fn_idx], c='red', s=3, label='pos')
 ax.scatter(scores[fp_idx | tn_idx], labels[fp_idx | tn_idx] + noise[fp_idx | tn_idx], c='blue', s=3, label='neg')
 ax.set_xlabel('Score')
@@ -68,13 +64,13 @@ sns.kdeplot(x=scores[fp_idx | tn_idx], ax=ax1, c='blue')
 
 ax.legend(loc='lower right')
 
-ax.savefig(os.path.join(plot_dir, 'dwd_scores.png'))
+ax.savefig(os.path.join(plot_dir, 'wdwd_scores.png'))
 
 # mark misclassified objects
 n = len(ids)
-noise = np.random.RandomState(10).normal(scale=.1, size=n)
+noise = (np.arange(n) - n // 2) / 5000
 ax = plt.axes()
-ax.set_title('Scatterplot of DWD scores (jitter = N(0, 0.1))', fontsize=13, fontweight='bold')
+ax.set_title('Scatterplot of wDWD scores (jitter = ID)', fontsize=13, fontweight='bold')
 ax.scatter(scores[tp_idx], labels[tp_idx] + noise[tp_idx], c='red', s=3, label='tp')
 ax.scatter(scores[tn_idx], labels[tn_idx] + noise[tn_idx], c='blue', s=3, label='tn')
 ax.scatter(scores[fn_idx], labels[fn_idx] + noise[fn_idx], c='green', s=3, label='fn')
@@ -90,7 +86,7 @@ ax.axvline((min(scores[tp_idx]) + max(scores[tn_idx])) / 2, c='black', ls='--')
 
 ax.legend(loc='lower right')
 
-ax.savefig(os.path.join(plot_dir, 'dwd_scores_misclf.png'))
+ax.savefig(os.path.join(plot_dir, 'wdwd_scores_misclf.png'))
 
 # extreme images
 tp_ids = ids[tp_idx][scores[tp_idx].argsort()]
@@ -98,29 +94,12 @@ fn_ids = ids[fn_idx][scores[fn_idx].argsort()]
 fp_ids = ids[fp_idx][scores[fp_idx].argsort()]
 tn_ids = ids[tn_idx][scores[tn_idx].argsort()]
 
-get_extreme_images(tp_ids)
-get_extreme_images(fn_ids)
-get_extreme_images(fp_ids)
-get_extreme_images(tn_ids)
+tp_dir = os.path.join(plot_dir, 'tp')
+fn_dir = os.path.join(plot_dir, 'fn')
+fp_dir = os.path.join(plot_dir, 'fp')
+tn_dir = os.path.join(plot_dir, 'tn')
 
-
-def get_extreme_images(ids, save_dir, n_subjects=9):
-    left_ext_ids, right_ext_ids = ids[:n_subjects], ids[-n_subjects:]
-    plot_images(left_ext_ids)
-
-
-def plot_images(ids, save_name):
-    n = len(ids)
-    # generate h x 3 subplot grid
-    h = n // 3
-    fig, axs = plt.subplots(3, h, figsize=(10 * 3, 10 * h))
-    for i, ax in enumerate(axs.flat):
-        if i >= n:
-            continue
-        file = glob(os.path.join(Paths().images_dir,
-                                 image_type.lower(),
-                                 ids[i] + '_core*'))[0]
-        img = imread(file)
-        ax.imshow(img)
-        ax.set_xlabel('{}'.format(ids[i]))
-    fig.savefig(save_name)
+get_extreme_images(tp_ids, tp_dir)
+get_extreme_images(fn_ids, fn_dir)
+get_extreme_images(fp_ids, fp_dir)
+get_extreme_images(tn_ids, tn_dir)
--- tma_ajive/viz_utils.py
+++ tma_ajive/viz_utils.py
@@ -14,7 +14,7 @@ def mpl_noaxis(labels=False):
     """
     Do not display any axes for any figure.
     """
-    
+
     mpl.rcParams['axes.linewidth'] = 0
 
     if not labels:
@@ -23,3 +23,28 @@ def mpl_noaxis(labels=False):
 
         mpl.rcParams['ytick.left'] = False
         mpl.rcParams['ytick.labelleft'] = 0
+
+
+def get_extreme_images(ids, save_dir, n_subjects=9):
+    left_ext_ids, right_ext_ids = ids[:n_subjects], ids[-n_subjects:]
+    left_file = os.path.join(save_dir, 'left')
+    right_file = os.path.join(save_dir, 'right')
+    plot_images(left_ext_ids, left_file)
+    plot_images(right_ext_ids, right_file)
+
+
+def plot_images(ids, save_file):
+    n = len(ids)
+    # generate h x 3 subplot grid
+    h = n // 3
+    fig, axs = plt.subplots(3, h, figsize=(10 * 3, 10 * h))
+    for i, ax in enumerate(axs.flat):
+        if i >= n:
+            continue
+        file = glob(os.path.join(Paths().images_dir,
+                                 image_type.lower(),
+                                 ids[i] + '_core*'))[0]
+        img = imread(file)
+        ax.imshow(img)
+        ax.set_xlabel('{}'.format(ids[i]))
+    fig.savefig(save_file)
