59828252ac8d2e3a70f3c5afc6f7a1a2f7b52bfe
Taebin Kim <44902482+taebinkim7@users.noreply.github.com>
2021-09-08 15:59:14 -0400

    Update files

--- tma_ajive/viz_utils.py
+++ tma_ajive/viz_utils.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 
 from glob import glob
+from math import ceil
 from skimage.io import imread
 from tma_ajive.Paths import Paths
 
@@ -30,21 +31,41 @@ def mpl_noaxis(labels=False):
         mpl.rcParams['ytick.labelleft'] = 0
 
 
-def get_extreme_images(ids, image_type, save_dir, n_subjects=9):
+def get_extreme_images(ids, image_type, save_dir, n_subjects=9, plot_all=True):
     os.makedirs(save_dir, exist_ok=True)
     n_subjects = min(n_subjects, len(ids) // 2)
     left_ext_ids, right_ext_ids = ids[:n_subjects], ids[-n_subjects:]
     left_file = os.path.join(save_dir, 'left')
     right_file = os.path.join(save_dir, 'right')
-    plot_images(left_ext_ids, image_type, left_file)
-    plot_images(right_ext_ids, image_type, right_file)
+    if all_images:
+        plot_all_images(left_ext_ids, image_type, left_file)
+        plot_all_images(right_ext_ids, image_type, right_file)
+    else:
+        plot_images(left_ext_ids, image_type, left_file)
+        plot_images(right_ext_ids, image_type, right_file)
 
+def plot_all_images(ids, image_type, save_file):
+    n = len(ids)
+    # generate h x 3 subplot grid
+    fig, axs = plt.subplots(nrows=3, ncols=n, figsize=(10 * n, 10 * 3))
+    for i in range(n):
+        files = glob(os.path.join(Paths().images_dir,
+                                  image_type.lower(),
+                                  ids[i] + '_core*'))
+        for j, file in enumerate(files):
+            ax = axs[j, i]
+            img = imread(file)
+            ax.imshow(img)
+            ax.set_xlabel('{}'.format(ids[i]), fontsize=30)
+            ax.tick_params(top=False, bottom=False, left=False, right=False,
+                           labelleft=False, labelbottom=False)
+        fig.savefig(save_file)
 
 def plot_images(ids, image_type, save_file):
     n = len(ids)
     # generate h x 3 subplot grid
-    h = n // 3
-    fig, axs = plt.subplots(3, h, figsize=(10 * 3, 10 * h))
+    h = ceil(n / 3)
+    fig, axs = plt.subplots(nrows=3, ncols=h, figsize=(10 * h, 10 * 3))
     for i, ax in enumerate(axs.flat):
         if i >= n:
             continue
