4f69a2a57cc818c7fa03cb6eedc36666bdfabd75
Taebin Kim <44902482+taebinkim7@users.noreply.github.com>
2021-09-08 17:13:12 -0400

    Update files

--- tma_ajive/viz_utils.py
+++ tma_ajive/viz_utils.py
@@ -63,8 +63,8 @@ def plot_all_images(ids, image_type, save_file):
             else:
                 img = imread(file)
                 ax.imshow(img)
-                ax.set_xlabel('{}'.format(os.path.basename(file[:-4])),
-                              fontsize=30)
+                ax.set_xlabel('{}'.format(os.path.basename(file[:-7])),
+                              fontsize=25)
                 ax.tick_params(top=False, bottom=False, left=False, right=False,
                                labelleft=False, labelbottom=False)
         fig.savefig(save_file)
