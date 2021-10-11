10821ff6fe4f39e2f572e2e20a4e95c6d64cfaec
Taebin Kim <44902482+taebinkim7@users.noreply.github.com>
2021-10-11 14:14:17 -0400

    Fix error

--- scripts/clf_er.py
+++ scripts/clf_er.py
@@ -20,7 +20,7 @@ args = parser.parse_args()
 data_dir = os.path.join('/datastore/nextgenout5/share/labs/smarronlab/tkim/data', args.data_dir)
 paths = Paths(data_dir)
 
-data = load_analysis_data(paths=paths, level=args.level, types=['feats_er', args.type])
+data = load_analysis_data(paths=paths, level=args.level, types=['feats_er', args.target])
 feats_er = data['feats_er']
 if 'surv' in args.target: # e.g., surv_3yrs
     k = int(args.target.split('_')[1][0])
