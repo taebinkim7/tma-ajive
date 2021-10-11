abce80c0712fb701cf63d01fd9779f457a435f73
Taebin Kim <44902482+taebinkim7@users.noreply.github.com>
2021-10-11 03:53:21 -0400

    Update load analysis data

--- tma_ajive/load_analysis_data.py
+++ tma_ajive/load_analysis_data.py
@@ -9,22 +9,39 @@ from tma_ajive.Paths import Paths
 from tma_ajive.utils import retain_pandas
 
 
-def load_analysis_data(paths=Paths(), level='subj', load_patch_data=False):
+def load_analysis_data(paths=Paths(), level='subj',
+                       types=['feats_he', 'feats_er', 'labels_er'],
+                       load_patch_data=False):
+
+    # define dictionary to return
+    data = {}
 
     ##############
     # image data #
     ##############
 
     feats_dir = paths.features_dir
-    labels_dir = paths.classification_dir
+    clf_dir = paths.classification_dir
 
     # image features
-    feats_he = pd.read_csv(os.path.join(feats_dir,
-        level + '_features_he.csv'), index_col=0)
-    feats_er = pd.read_csv(os.path.join(feats_dir,
-        level + '_features_er.csv'), index_col=0)
-    labels_er = pd.read_csv(os.path.join(labels_dir,
-        level + '_labels_er.csv'), index_col=0)
+    if 'feats_he' in types:
+        feats_he = pd.read_csv(os.path.join(feats_dir,
+            level + '_features_he.csv'), index_col=0)
+        data['feats_he'] = feats_he
+    if 'feats_er' in types:
+        feats_er = pd.read_csv(os.path.join(feats_dir,
+            level + '_features_er.csv'), index_col=0)
+        data['feats_er'] = feats_er
+
+    # clinical data
+    if 'labels_er' in types:
+        labels_er = pd.read_csv(os.path.join(clf_dir,
+            level + '_labels_er.csv'), index_col=0)
+        data['labels_er'] = labels_er
+    if 'surv_mos' in types:
+        surv_mos = pd.read_csv(os.path.join(clf_dir,
+            level + '_surv_mos.csv'), index_col=0)
+        data['surv_mos'] = surv_mos
 
     # patch data
     if load_patch_data:
@@ -39,40 +56,28 @@ def load_analysis_data(paths=Paths(), level='subj', load_patch_data=False):
         patch_feats_er = \
             pd.read_csv(os.path.join(feats_dir, 'patch_features_er.csv'),
                         index_col=['image', 'patch_idx'])
-    else:
-        patch_dataset_he, patch_dataset_er, patch_feats_he, patch_feats_er = \
-            None, None, None, None
+        data['patch_dataset_he'] = patch_dataset_he
+        data['patch_dataset_er'] = patch_dataset_er
+        data['patch_feats_he'] = patch_feats_he
+        data['patch_feats_er'] = patch_feats_er
 
     # process data
     image_feats_processor = StandardScaler()
+    data['image_feats_processor'] = image_feats_processor
     feats_he = retain_pandas(feats_he, image_feats_processor.fit_transform)
     feats_er = retain_pandas(feats_er, image_feats_processor.fit_transform)
 
     #############
     # alignment #
     #############
-    intersection = list(set(feats_he.index)\
-        .intersection(set(feats_er.index))\
-        .intersection(set(labels_er.index)))
+    index_sets = list(map(lambda x: set(x.index), list(data.values())))
+    intersection = set.intersection(*index_sets)
     intersection.sort()
 
     print('No. intersection: {}'.format(len(intersection)))
 
-    feats_he = feats_he.loc[intersection]
-    feats_er = feats_er.loc[intersection]
-    labels_er = labels_er.loc[intersection]
+    for type in data:
+        data[type] = data[type].loc[intersection]
 
-    # sort by index
-    # idx = feats_he.index.sort_values()
-    # feats_he = feats_he.loc[idx]
-    # feats_er = feats_er.loc[idx]
-    # labels_er = labels_er.loc[idx]
 
-    return {'feats_he': feats_he,
-            'feats_er': feats_er,
-            'labels_er' : labels_er,
-            'image_feats_processor': image_feats_processor,
-            'patch_dataset_he': patch_dataset_he,
-            'patch_dataset_er': patch_dataset_er,
-            'patch_feats_he': patch_feats_he,
-            'patch_feats_er': patch_feats_er}
+    return data
