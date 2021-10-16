2ef83ff690910dec4a3b106d78e7a5d3f104f88b
Taebin Kim <44902482+taebinkim7@users.noreply.github.com>
2021-10-16 12:20:31 -0400

    Update files

--- scripts/clf_he.py
+++ scripts/clf_he.py
@@ -14,14 +14,25 @@ parser = ArgumentParser()
 parser.add_argument('--data_dir', type=str, required=True)
 parser.add_argument('--level', type=str, default='subj')
 parser.add_argument('--iter', type=int, default=10)
+parser.add_argument('--target', type=str, default='labels_er')
 args = parser.parse_args()
 
 data_dir = os.path.join('/datastore/nextgenout5/share/labs/smarronlab/tkim/data', args.data_dir)
 paths = Paths(data_dir)
 
-data = load_analysis_data(paths=paths, level=args.level)
-feats_he = data['feats_he']
-labels = data['labels_er']
+if 'surv' in args.target: # e.g., surv_3yrs
+    data = load_analysis_data(paths=paths, level=args.level,
+                              types=['feats_he', 'surv_mos'])
+    feats_he = data['feats_he']
+    k = int(args.target.split('_')[1][0])
+    labels = data['surv_mos']
+    labels[args.target] = [int(mo > 12 * k) for mo in labels['surv_mos']]
+    labels = labels.drop(columns=['surv_mos'])
+else:
+    data = load_analysis_data(paths=paths, level=args.level,
+                              types=['feats_he', args.target])
+    feats_he = data['feats_he']
+    labels = data[args.target]
 
 metrics_list = []
 for i in tqdm(range(10)):
