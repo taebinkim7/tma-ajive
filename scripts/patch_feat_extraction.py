8c14a1af84d36781f78314d46ae2f746089f3dc9
Taebin Kim <44902482+taebinkim7@users.noreply.github.com>
2021-10-21 11:04:57 -0400

    Add efficientnet-b7

--- scripts/patch_feat_extraction.py
+++ scripts/patch_feat_extraction.py
@@ -8,6 +8,7 @@ from tma_ajive.Paths import Paths
 parser = ArgumentParser()
 parser.add_argument('--data_dir', type=str, required=True)
 parser.add_argument('--patch_size', type=int, default=200)
+parser.add_argument('--model', type=str, default='vgg16')
 parser.add_argument('--image_types', type=str, nargs='+', default=['he', 'er'])
 args = parser.parse_args()
 
@@ -17,4 +18,5 @@ paths = Paths(data_dir)
 for type in args.image_types:
     patch_feat_extraction(paths=paths,
                           image_type=type,
-                          patch_size=args.patch_size)
+                          patch_size=args.patch_size,
+                          pretrained_model=args.model)
